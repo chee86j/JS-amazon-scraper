d5996064410a42d6045b2d5cc2028f5e279f89ee
chee86j <jeffreychee86@gmail.com>
2024-07-05 23:56:37 -0400

    added averageRating and reviewCount

--- scrapers.js
+++ scrapers.js
@@ -3,8 +3,13 @@ const puppeteer = require("puppeteer");
 async function scrapeProduct(url) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(url);
+  await page.goto(url, { waitUntil: "domcontentloaded" });
 
+  /* page.evaluate() method is used to run a script in the context of the page instead of the more complicated 'page.$x'
+     requiring XPath expressions. This method can be used to extract data from the page, but it can't return DOM elements.
+     The choice between using page.evaluate() and page.$x() depends on the complexity of the script you want to run. For most
+     cases, page.evaluate() is the best choice. However, if you need to extract a single element, page.$x() is more suitable.
+  */
   const productData = await page.evaluate(() => {
     const title = document.getElementById("productTitle")
       ? document.getElementById("productTitle").innerText.trim()
@@ -15,13 +20,22 @@ async function scrapeProduct(url) {
     const imageSrc = document.getElementById("landingImage")
       ? document.getElementById("landingImage").src
       : "Image source not found";
+    const averageRating = document.getElementById("acrPopover")
+      ? document.getElementById("acrPopover").title
+      : "Rating not found";
+    const reviewCount = document.getElementById("acrCustomerReviewText")
+      ? document.getElementById("acrCustomerReviewText").innerText.trim()
+      : "Review count not found";
 
-    return { title, price, imageSrc };
+    return { title, price, imageSrc, averageRating, reviewCount };
   });
 
   console.log(productData);
+
+  await browser.close();
 }
 
-scrapeProduct(
-  "https://www.amazon.com/Portable-Stroller-12000mAh-Operated-Flexible/dp/B08ZNBTVSG/ref=sr_1_1?dib=eyJ2IjoiMSJ9.4n1QfFNXvbnKRxds-Zo190HqCBSKsnjVMRfUixKCwk4lWqDgESgWa-_G_qRQ7Sr_Mirv6GDaWuCAizpfOwVER7w7r26-kr8ZsZxv76lkbyQ.aD0alT1ANX8q-OyRyy6SqmfVou8Xr5o2J0A6LsKXM5A&dib_tag=se&keywords=stroller%2Bfan%2Bfrizcol&qid=1720139460&refinements=p_123%3A956916&rnid=85457740011&s=baby-products&sr=1-1&th=1"
-);
+// URL of the product
+const productURL =
+  "https://www.amazon.com/Portable-Stroller-12000mAh-Operated-Flexible/dp/B08ZNBTVSG";
+scrapeProduct(productURL);
